9858ae2269e793a4fd1888654d80784a3f379727
ZJie1 <52772496+ZJie1@users.noreply.github.com>
2020-04-16 10:53:56 +0800

    [POAE7-128] Add the support of version CDP 7.0 in columnar_format_qat_wrapper (#12)

--- columnar_format_qat_wrapper/apply_hive_jars.sh
+++ columnar_format_qat_wrapper/apply_hive_jars.sh
@@ -17,14 +17,31 @@
 # */
 #!/bin/bash
 
-declare -a supported_CDH_versions=("5.14.2")
-declare -A cdh_parquet_format_version_m=( ["5.14.2"]="2.1.0")
-declare -A cdh_parquet_mr_version_m=( ["5.14.2"]="1.5.0")
-declare -A cdh_hive_version_m=( ["5.14.2"]="1.1.0")
+declare -a supported_CDH_versions=("5.14.2" "6.2.1" "7.0.0")
+declare -A cdh_parquet_format_version_m=( ["5.14.2"]="2.1.0" ["7.0.0"]="2.4.0")
+declare -A cdh_parquet_mr_version_m=( ["5.14.2"]="1.5.0" ["7.0.0"]="1.10.0")
+declare -A cdp_orc_version_m=(["7.0.0"]="1.5.1")
+declare -A cdh_hive_version_m=( ["5.14.2"]="1.1.0" ["7.0.0"]="3.1.0")
 # Repo Address
 PARQUET_MR_REPO=https://github.com/cloudera/parquet-mr
 PARQUET_FORMAT_REPO=https://github.com/cloudera/parquet-format
 HIVE_REPO=https://github.com/cloudera/hive
+
+##upstream Repo Address
+UPSTREAM_PARQUET_MR_REPO=https://github.com/apache/parquet-mr.git
+UPSTREAM_PARQUET_FORMAT_REPO=https://github.com/apache/parquet-format.git
+UPSTREAM_ORC_REPO=https://github.com/apache/orc.git
+UPSTREAM_HIVE_REPO=https://github.com/apache/hive.git
+
+CDH_release_version=$1
+QATCodec_SRC_DIR=$2
+HIVE_QAT_DIR=$QATCodec_SRC_DIR/columnar_format_qat_wrapper
+TARGET_DIR=$HIVE_QAT_DIR/target
+
+ORC_SRC_DIR=${TARGET_DIR}/orc
+HIVE_SRC_DIR=${TARGET_DIR}/hive
+PARQUET_MR_SRC_DIR=${TARGET_DIR}/parquet-mr
+PARQUET_FORMAT_SRC_DIR=${TARGET_DIR}/parquet-format
 
 function clone_repo(){
   echo "Clone Branch $1 from Repo $2"
@@ -38,7 +55,7 @@ function usage(){
 
 function check_CDH_version(){
   valid_version=false
-  for v in $supported_CDH_versions
+  for v in ${supported_CDH_versions[@]}
   do
   	if [ "$v" =  "$1" ]; then
 		valid_version=true
@@ -51,43 +68,115 @@ function check_CDH_version(){
   fi
 }
 
+apply_patch_to_cdp_orc(){
+   pushd $TARGET_DIR
+   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
+   if [ "$CDH_major_version" = "7" ]; then
+      ORC_BRANCH="rel/release-${cdp_orc_version_m[$CDH_release_version]}"
+   fi
+   clone_repo $ORC_BRANCH $UPSTREAM_ORC_REPO
+   popd
+   apply_diff_to_orc
+}
+
+apply_diff_to_orc(){
+   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
+   if [ "$CDH_major_version" = "7" ]; then
+      pushd $ORC_SRC_DIR
+      git apply --reject --whitespace=fix $HIVE_QAT_DIR/$CDH_release_version/orc/*.diff
+      popd
+   fi
+}
+
 apply_patch_to_cdh_hive(){
   pushd $TARGET_DIR
   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
-  HIVE_BRANCH="cdh$CDH_major_version-${cdh_hive_version_m[$CDH_release_version]}_$CDH_release_version"
+  if [ "$CDH_major_version" = "6" ]; then
+    HIVE_BRANCH="cdh$CDH_release_version"
+  elif [ "$CDH_major_version" = "7" ]; then
+    HIVE_BRANCH="rel/release-${cdh_hive_version_m[$CDH_release_version]}"
+    clone_repo $HIVE_BRANCH $UPSTREAM_HIVE_REPO
+    popd
+    apply_diff_to_hive
+    return
+  else
+    HIVE_BRANCH="cdh$CDH_major_version-${cdh_hive_version_m[$CDH_release_version]}_$CDH_release_version"
+  fi
   clone_repo $HIVE_BRANCH $HIVE_REPO
   echo yes | cp -rf $HIVE_QAT_DIR/$CDH_release_version/hive $TARGET_DIR/
   popd
 }
 
+apply_diff_to_hive(){
+   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
+   if [ "$CDH_major_version" = "7" ]; then
+      pushd $HIVE_SRC_DIR
+      git apply --reject --whitespace=fix $HIVE_QAT_DIR/$CDH_release_version/hive/*.diff
+      popd
+   fi
+}
+
 apply_patch_to_cdh_parquet_format(){
   pushd $TARGET_DIR
   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
-  PARQUET_FORMAT_BRANCH="cdh$CDH_major_version-${cdh_parquet_format_version_m[$CDH_release_version]}_$CDH_release_version"
+  if [ "$CDH_major_version" = "6" ]; then
+    PARQUET_FORMAT_BRANCH="cdh$CDH_release_version"
+  elif [ "$CDH_major_version" = "7" ]; then
+    PARQUET_FORMAT_BRANCH="apache-parquet-format-${cdh_parquet_format_version_m[$CDH_release_version]}"
+    clone_repo $PARQUET_FORMAT_BRANCH $UPSTREAM_PARQUET_FORMAT_REPO
+    popd
+    apply_diff_to_parquet_format
+    return
+  else
+    PARQUET_FORMAT_BRANCH="cdh$CDH_major_version-${cdh_parquet_format_version_m[$CDH_release_version]}_$CDH_release_version"
+  fi
   clone_repo $PARQUET_FORMAT_BRANCH $PARQUET_FORMAT_REPO
   echo yes | cp -rf $HIVE_QAT_DIR/$CDH_release_version/parquet-format $TARGET_DIR/
   popd
 }
 
+apply_diff_to_parquet_format(){
+   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
+   if [ "$CDH_major_version" = "7" ]; then
+      pushd $PARQUET_FORMAT_SRC_DIR
+      git apply --reject --whitespace=fix $HIVE_QAT_DIR/$CDH_release_version/parquet-format/*.diff
+      popd
+   fi
+}
+
 apply_patch_to_cdh_parquet_mr(){
   pushd $TARGET_DIR
   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
-  PARQUET_MR_BRANCH="cdh$CDH_major_version-${cdh_parquet_mr_version_m[$CDH_release_version]}_$CDH_release_version"
+  if [ "$CDH_major_version" = "6" ]; then
+    PARQUET_MR_BRANCH="cdh$CDH_release_version"
+  elif [ "$CDH_major_version" = "7" ]; then
+    PARQUET_MR_BRANCH="apache-parquet-${cdh_parquet_mr_version_m[$CDH_release_version]}"
+    clone_repo $PARQUET_MR_BRANCH $UPSTREAM_PARQUET_MR_REPO
+    popd
+    apply_diff_to_parquet_mr
+    return
+  else
+    PARQUET_MR_BRANCH="cdh$CDH_major_version-${cdh_parquet_mr_version_m[$CDH_release_version]}_$CDH_release_version"
+  fi
   clone_repo $PARQUET_MR_BRANCH $PARQUET_MR_REPO
   echo yes | cp -rf $HIVE_QAT_DIR/$CDH_release_version/parquet-mr $TARGET_DIR/
   popd
+}
+
+apply_diff_to_parquet_mr(){
+   CDH_major_version=$(echo $CDH_release_version | cut -d '.' -f 1)
+   if [ "$CDH_major_version" = "7" ]; then
+      pushd $PARQUET_MR_SRC_DIR
+      git apply --reject --whitespace=fix $HIVE_QAT_DIR/$CDH_release_version/parquet-mr/*.diff
+      popd
+   fi
 }
 
 if [ "$#" -ne 2 ]; then
   usage
 fi
 
-CDH_release_version=$1
 check_CDH_version $CDH_release_version
-
-QATCodec_SRC_DIR=$2
-HIVE_QAT_DIR=$QATCodec_SRC_DIR/columnar_format_qat_wrapper
-TARGET_DIR=$HIVE_QAT_DIR/target
 
 if [ -d $TARGET_DIR ]; then
   echo "$TARGET_DIR is not clean"
@@ -97,4 +186,5 @@ fi
 
 apply_patch_to_cdh_parquet_format
 apply_patch_to_cdh_parquet_mr
+apply_patch_to_cdp_orc
 apply_patch_to_cdh_hive
